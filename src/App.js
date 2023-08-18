b81f879c13176515a70526f736d8cf5b97f6c270
Joshua Sigston <joshua.sigston@outlook.com>
2023-08-18 14:53:24 -0600

    file changes

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ import Search from './components/Search';
 import Hamburger from './components/Hamburger';
 
 import { BrowserRouter, Link } from 'react-router-dom';
-import {GiCampCookingPot} from 'react-icons/gi'
+import { GiCampCookingPot } from 'react-icons/gi';
 
 function App() {
-  const [hamburger, setHamburger] = useState(false)
-  const [recipeList, setRecipeList] = useState([])
-  console.log(recipeList)
+  const [hamburger, setHamburger] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  console.log(recipeList);
   const handleToggleNav = () => {
-    setHamburger(prevState => !prevState)
-  }
+    setHamburger((prevState) => !prevState);
+  };
 
-  const toggleNav = {left: !hamburger ? '100%' : '0'}
+  const toggleNav = { left: !hamburger ? '100%' : '0' };
 
   return (
     <div className="App">
       <BrowserRouter>
-      <header>
-        <div className='left_col'>
-          <Link to={'/'}>
-            <GiCampCookingPot className='logo'/>
-          </Link>
-          <Link to={'/saved-recipes'}>
-            <h3>See Saved Recipes</h3>
-          </Link>
-        </div>
-        <div className='right_col'>
-          <Search />
-        </div>
-      </header>
-        {/* <Category handleToggleNav={handleToggleNav} style={toggleNav}/> */}
-        <Pages recipeList={recipeList} setRecipeList={setRecipeList}/>
+        <header>
+          <div className="left_col">
+            <Link to={'/'}>
+              <GiCampCookingPot className="logo" />
+            </Link>
+            <Link to={'/saved-recipes'}>
+              <h3>See Saved Recipes</h3>
+            </Link>
+          </div>
+          <div className="right_col">
+            <Search />
+          </div>
+        </header>
+        <Category handleToggleNav={handleToggleNav} style={toggleNav} />
+        <Pages recipeList={recipeList} setRecipeList={setRecipeList} />
         <Hamburger handleToggleNav={handleToggleNav} />
       </BrowserRouter>
     </div>
